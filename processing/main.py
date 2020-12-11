45e55429cac76f876ffe6601d16c961d90eefc30
hcwinsemius <h.c.winsemius@gmail.com>
2020-12-11 21:40:37 +0100

    start task upload_file and extract_frames #1

--- processing/main.py
+++ processing/main.py
@@ -5,37 +5,47 @@ import os
 import json
 import boto3
 import io
+import tasks
 
 # Callback function for each process task that is queued.
 def process(ch, method, properties, body):
     try:
         taskInput = json.loads(body.decode("utf-8"))
-        print('Process task of type %s' % taskInput['type'])
-
-        # Example request to API (only used for posting/updating information).
-        r = requests.get('http://portal/api/sites')
-
-        # Example upload file to S3.
-        s3 = boto3.resource('s3',
-                            endpoint_url='http://storage:9000',
-                            aws_access_key_id=os.getenv('S3_ACCESS_KEY'),
-                            aws_secret_access_key=os.getenv('S3_ACCESS_SECRET'),
-                            config=boto3.session.Config(signature_version='s3v4')
-                            )
-
-        # Create bucket if it doesn't exist yet.
-        if s3.Bucket('test-bucket') not in s3.buckets.all():
-            s3.create_bucket(Bucket='test-bucket')
-
-        uploadTestFile = io.BytesIO()
-        # Seek beginning of in-memory file before storing.
-        uploadTestFile.seek(0)
-        s3.Object('test-bucket', 'test.jpg').put(Body=uploadTestFile)
-
-        # Example download file from S3.
-        downloadTestFile = io.BytesIO()
-        s3.Object('test-bucket', 'test.jpg').download_fileobj(downloadTestFile)
-
+        task_name = taskInput['type']
+        kwargs = taskInput['kwargs']
+        if hasattr(tasks, task_name):
+            task = getattr(tasks, task_name)
+            print('Process task of type %s' % taskInput['type'])
+            try:
+                print(kwargs)
+                task(**kwargs)
+                r = 200
+            except:
+                r = 500
+        # # Example request to API (only used for posting/updating information).
+        # r = requests.get('http://portal/api/sites')
+        #
+        # # Example upload file to S3.
+        # s3 = boto3.resource('s3',
+        #                     endpoint_url='http://storage:9000',
+        #                     aws_access_key_id=os.getenv('S3_ACCESS_KEY'),
+        #                     aws_secret_access_key=os.getenv('S3_ACCESS_SECRET'),
+        #                     config=boto3.session.Config(signature_version='s3v4')
+        #                     )
+        #
+        # # Create bucket if it doesn't exist yet.
+        # if s3.Bucket('test-bucket') not in s3.buckets.all():
+        #     s3.create_bucket(Bucket='test-bucket')
+        #
+        # uploadTestFile = io.BytesIO()
+        # # Seek beginning of in-memory file before storing.
+        # uploadTestFile.seek(0)
+        # s3.Object('test-bucket', 'test.jpg').put(Body=uploadTestFile)
+        #
+        # # Example download file from S3.
+        # downloadTestFile = io.BytesIO()
+        # s3.Object('test-bucket', 'test.jpg').download_fileobj(downloadTestFile)
+        #
         # Acknowledge queue item at end of task.
         ch.basic_ack(delivery_tag=method.delivery_tag)
     except Exception as e:
@@ -57,3 +67,4 @@ except Exception as e:
     channel.stop_consuming()
     connection.close()
     traceback.print_tb(e.__traceback__)
+
---
+++ processing/tasks.py
@@ -0,0 +1,102 @@
+import os
+import boto3
+import OpenRiverCam
+import logging
+import io
+
+def upload_file(fn, bucket, dest=None):
+    """
+    Uploads BytesIO obj representation of data in file 'fn' in bucket
+    :param fn: str, full local path to file containing movie
+    :param bucket: str, name of bucket, if it does not exist, it will be created
+    :param dest=None: str, name of file in bucket, if left as None, the file name is stripped from fn
+
+    :return:
+    """
+    if dest is None:
+        dest = os.path.split(os.path.abspath(fn))[1]
+    # TODO: finalize implementation
+    # Example upload file to S3.
+    s3 = boto3.resource('s3',
+                        endpoint_url='http://storage:9000',
+                        aws_access_key_id=os.getenv('S3_ACCESS_KEY'),
+                        aws_secret_access_key=os.getenv('S3_ACCESS_SECRET'),
+                        config=boto3.session.Config(signature_version='s3v4')
+                        )
+
+    # Create bucket if it doesn't exist yet.
+    if s3.Bucket(bucket) not in s3.buckets.all():
+        s3.create_bucket(Bucket=bucket)
+    with open(fn, "rb") as f:
+        buf = io.BytesIO(f.read())
+    # Seek beginning of in-memory file before storing.
+    buf.seek(0)
+    s3.Object(bucket, dest).put(Body=buf)
+    print("Uploading is done!")
+
+
+
+def extract_frames(movie, camera, dest, prefix="frame_", logger=logging):
+    # open S3 bucket
+    s3 = boto3.resource('s3',
+                        endpoint_url='http://storage:9000',
+                        aws_access_key_id=os.getenv('S3_ACCESS_KEY'),
+                        aws_secret_access_key=os.getenv('S3_ACCESS_SECRET'),
+                        config=boto3.session.Config(signature_version='s3v4')
+                        )
+
+    # Create bucket if it doesn't exist yet.
+    if s3.Bucket('test-bucket') not in s3.buckets.all():
+        s3.create_bucket(Bucket='test-bucket')
+
+
+    n = 0
+    fns = []
+    t = []
+    logger.info(f"Writing movie {movie['file']['identifier']} to {dest}")
+    # open file from bucket in memory
+    mov = io.BytesIO()
+    s3.Object(movie["file"]["bucket"], movie["file"]["identifier"]).download_fileobj(mov)
+    with open(movie["file"]["identifier"], 'wb') as f:
+        f.write(mov)
+    for _t, buf in OpenRiverCam.io.frames(movie["file"]["identifier"], lens_pars=camera["lensParameters"]):
+        # Bucket filename
+        dest_fn = os.path.join(dest, '{:s}{:04d}.jpg'.format(prefix, n))
+        logger.debug(f"Write frame {n} in {dest_fn} to S3")
+        # Seek beginning of bytestream
+        buf.seek(0)
+        # Put file in bucket
+        s3.Object(movie["file"]["bucket"], dest_fn).put(Body=buf)
+        n += 1
+        # add file and timestamp to list
+        t += _t
+        fns += dest_fn
+
+    # TODO: Post status code on specific end point
+    # requests.post("http://.....", msg)
+    # r = 200  # Rick, agree on format for response body. I assume an error code (success: 200), and a data body
+    return 200
+#     "type": "extract_snapshots",
+#     "kwargs": {
+#         "movie": {
+#             "file": {
+#                 "bucket": "test-bucket",
+#                 "identifier": "schedule_20201120_142304.mkv"
+#             }
+#         },
+#         "camera": {
+#             "name": "Foscam E9900P",
+#             "configuration": {},
+#             "lensParameters": {
+#                 "k": 0.5
+#             }
+#         }
+#     }
+#
+# }
+#
+#     lens_pars = {
+#         "k1": -10.0e-6,
+#         "c": 2,
+#         "f": 8.0,
+#     }
