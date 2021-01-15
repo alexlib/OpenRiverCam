461403fc9bebd7ec97566d54ed479f0ebe62ce74
hcwinsemius <h.c.winsemius@gmail.com>
2021-01-15 17:03:06 +0100

    Q integration over depth and width #16 doc-strings updated black reformatting

--- processing/tasks.py
+++ processing/tasks.py
@@ -9,12 +9,14 @@ from datetime import datetime, timedelta
 from shapely.geometry import shape
 from rasterio.plot import reshape_as_raster
 
+
 def upload_file(fn, bucket, dest=None, logger=logging):
     """
     Uploads BytesIO obj representation of data in file 'fn' in bucket
     :param fn: str, full local path to file containing movie
     :param bucket: str, name of bucket, if it does not exist, it will be created
     :param dest=None: str, name of file in bucket, if left as None, the file name is stripped from fn
+    :param logger=logging: logger-object
 
     :return:
     """
@@ -34,8 +36,8 @@ def extract_frames(movie, camera, prefix="frame", logger=logging):
     Extract raw frames, only lens correct using camera lensParameters, and store in RGB photos
     :param movie: dict containing movie information
     :param camera: dict, camera properties, such as lensParameters, name
-    :param prefix: str, prefix of file names, used in storage bucket
-    :param logger:
+    :param prefix="frame": str, prefix of file names, used in storage bucket, normally not changed by user
+    :param logger=logging: logger-object
     :return:
     """
     # open S3 bucket
@@ -73,8 +75,8 @@ def extract_project_frames(movie, prefix="proj", logger=logging):
     Extract frames, lens correct, greyscale correct and project to defined AOI with GCPs, water level and camera position
     Results in GeoTIFF files in desired projection and resolution within bucket defined in movie
     :param movie: dict, movie information
-    :param prefix="proj": str, prefix of file names, used in storage bucket
-    :param logger=logging: logging obj
+    :param prefix="proj": str, prefix of file names, used in storage bucket, normally not changed by user
+    :param logger=logging: logger-object
     :return:
     """
     # TODO check on time frame of movie and validity of the camera_config
@@ -137,8 +139,9 @@ def extract_project_frames(movie, prefix="proj", logger=logging):
 
 def get_aoi(camera_config, logger=logging):
     """
-
+    add the aoi dictionary to camera_config based on user inputs
     :param camera_config: camera configuration with ["aoi"]["bbox"] still missing
+    :param logger=logging: logger-object
     :return:
     """
     # some assertion
@@ -171,7 +174,8 @@ def get_aoi(camera_config, logger=logging):
 def compute_piv(movie, file, prefix="proj", piv_kwargs={}, logger=logging):
     """
     compute velocities over frame pairs, choosing frame interval, start / end frame.
-
+    :param movie: dict, contains file dictionary and camera_config
+    :param file: dict, contains file information for writing outputs
     :return:
     """
     var_names = ["v_x", "v_y", "s2n"]
@@ -250,8 +254,18 @@ def compute_piv(movie, file, prefix="proj", piv_kwargs={}, logger=logging):
     # prepare local axes
     spacing_x = np.diff(cols[0])[0]
     spacing_y = np.diff(rows[:, 0])[0]
-    x = np.linspace(resolution / 2 * spacing_x, (len(cols[0]) - 0.5) * resolution * spacing_x, len(cols[0]))
-    y = np.flipud(np.linspace(resolution / 2 * spacing_y, (len(rows[:, 0]) - 0.5) * resolution * spacing_y, len(rows[:, 0])))
+    x = np.linspace(
+        resolution / 2 * spacing_x,
+        (len(cols[0]) - 0.5) * resolution * spacing_x,
+        len(cols[0]),
+    )
+    y = np.flipud(
+        np.linspace(
+            resolution / 2 * spacing_y,
+            (len(rows[:, 0]) - 0.5) * resolution * spacing_y,
+            len(rows[:, 0]),
+        )
+    )
 
     # prepare dataset
     dataset = OpenRiverCam.io.to_dataset(
@@ -276,10 +290,15 @@ def compute_piv(movie, file, prefix="proj", piv_kwargs={}, logger=logging):
     return 200
 
 
-def compute_q(velocity, bathymetry, logger=logging):
+def compute_q(
+    velocity, bathymetry, z_0, h_a, v_corr=0.85, quantile=0.5, logger=logging
+):
     """
-    compute velocities over provided bathymetric cross section points.
-    :param
+    compute velocities over provided bathymetric cross section points, depth integrated velocities and river flow
+    over several quantiles.
+    :param velocity: dict, contains bucket / identifier of NetCDF file with velocities as time series grids (time, x, y)
+    :param bathymetry: dict, contains site name (str), site CRS (int) and coords (list of [x, y, z]) of
+        bathymetry cross section
 
     :return:
     """
@@ -294,12 +313,33 @@ def compute_q(velocity, bathymetry, logger=logging):
     fn = velocity["file"]["identifier"]
     s3.Bucket(bucket).download_file(fn, "temp.nc")
 
+    # retrieve velocities over cross section only (ds_points has time, points as dimension)
     ds_points = OpenRiverCam.io.interp_coords("temp.nc", *zip(*bathymetry["coords"]))
+
+    # add the effective velocity perpendicular to cross-section
+    ds_points["v_eff"] = OpenRiverCam.piv.vector_to_scalar(
+        ds_points["v_x"], ds_points["v_y"]
+    )
+
+    # integrate over depth with vertical correction
+    ds_points["q"] = OpenRiverCam.piv.depth_integrate(
+        ds_points["zcoords"], ds_points["v_eff"], z_0, h_a, v_corr=v_corr
+    )
+
+    # now integrate over the width of the cross-section
+    Q = OpenRiverCam.piv.integrate_flow(ds_points["q"], quantile=quantile)
+
     # overwrite gridded netCDF with cross section netCDF
     ds_points.to_netcdf("temp.nc", encoding=encoding)
-    s3.Bucket(bucket).upload_file("temp.nc", "q.nc")
+    s3.Bucket(bucket).upload_file("temp.nc", "q_depth.nc")
+    logger.info(f"q_depth.nc successfully written in {bucket}")
+
+    # overwrite gridded netCDF with cross section netCDF
+    Q.to_netcdf("temp.nc", encoding=encoding)
+    s3.Bucket(bucket).upload_file("temp.nc", "Q.nc")
+
     os.remove("temp.nc")
-    logger.info(f"q.nc successfully written in {bucket}")
+    logger.info(f"Q.nc successfully written in {bucket}")
     # TODO: Post status code on specific end point (Rick)
     # requests.post("http://.....", msg)
     return 200
