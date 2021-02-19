72d5aced723315b931fa04175fc8b2bf96d849c5
Rick Hagenaars <rick_hagenaars@msn.com>
2021-02-19 18:50:30 +0100

    Altered list views with specific column and nest-relation filtering

--- portal/views/__init__.py
+++ portal/views/__init__.py
@@ -1,7 +1,9 @@
 import flask_admin as admin
 from flask_security import current_user
 from flask_admin.menu import MenuLink
+from flask_admin import expose
 from flask_admin.contrib.sqla import ModelView
+from flask_admin.contrib.sqla.filters import BaseSQLAFilter
 from flask import redirect, url_for
 
 # Models for CRUD views.
@@ -31,6 +33,33 @@ class UserModelView(ModelView):
         if not self.is_accessible():
             return redirect(url_for("security.login"))
 
+
+class CameraConfigView(UserModelView):
+    column_list = ("camera", CameraConfig.time_start, CameraConfig.time_end, CameraConfig.movie_setting_resolution, CameraConfig.movie_setting_fps)
+
+class FilterMovieBySite(BaseSQLAFilter):
+    # Override to create an appropriate query and apply a filter to said query with the passed value from the filter UI
+    def apply(self, query, value, alias=None):
+        return query.join(Movie.config).join(CameraConfig.camera).join(Camera.site).filter(Site.id == value)
+
+    # readable operation name. This appears in the middle filter line drop-down
+    def operation(self):
+        return u'equals'
+
+    # Override to provide the options for the filter - in this case it's a list of the titles of the Client model
+    def get_options(self, view):
+        return [(site.id, site.name) for site in Site.query.order_by(Site.name)]
+
+class MovieView(UserModelView):
+    column_list = ("config.camera.site", Movie.file_name, Movie.timestamp, Movie.actual_water_level, Movie.discharge, Movie.status)
+    column_labels = {"config.camera.site": "Site"}
+    column_filters = [FilterMovieBySite(column=None, name='Site')]
+
+    # Need this so the filter options are always up-to-date.
+    @expose('/')
+    def index_view(self):
+        self._refresh_filters_cache()
+        return super(MovieView, self).index_view()
 
 # Extend this view class for views which should only be available for logged in users.
 class UserView(admin.BaseView):
@@ -74,7 +103,7 @@ admin.add_view(
     UserModelView(Camera, db, name="Cameras", url="cameras", category="Setup")
 )
 admin.add_view(
-    UserModelView(
+    CameraConfigView(
         CameraConfig,
         db,
         name="Camera configuration",
@@ -82,7 +111,7 @@ admin.add_view(
         category="Setup",
     )
 )
-admin.add_view(UserModelView(Movie, db, name="Movies", url="movies"))
+admin.add_view(MovieView(Movie, db, name="Movies", url="movies"))
 
 # Custom user views.
 admin.add_view(RatingCurveView(name="Rating curves", url="ratingcurves"))
