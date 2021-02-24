c09b94e66d0a1d0296a10607da3e4c5329677c8c
Rick Hagenaars <rick_hagenaars@msn.com>
2021-02-24 13:20:20 +0100

    Send tasks over RabbitMQ when movie instance gets updated. Updated movie model.

--- portal/models/movie.py
+++ portal/models/movie.py
@@ -1,8 +1,12 @@
-from sqlalchemy import Integer, ForeignKey, String, Column, DateTime, Enum, Float, Text
+import os
+import pika
+import json
+import enum
+from sqlalchemy import event, Integer, ForeignKey, String, Column, DateTime, Enum, Float, Text
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.orm import relationship
-import enum
 from models.base import Base
+from models.example_data import movie as movie_example
 
 
 class MovieType(enum.Enum):
@@ -22,15 +26,19 @@ class Movie(Base, SerializerMixin):
     __tablename__ = "movie"
     id = Column(Integer, primary_key=True)
     config_id = Column(Integer, ForeignKey("configuration.id"))
-    file_bucket = Column(String)
-    file_name = Column(String)
+    file_bucket = Column(String, default="example")
+    file_name = Column(String, default="example_video.mp4")
     timestamp = Column(DateTime)
-    type = Column(Enum(MovieType))
+    type = Column(Enum(MovieType), default=MovieType.MOVIE_TYPE_NORMAL)
     actual_water_level = Column(Float)
     bathymetry_id = Column(Integer, ForeignKey("bathymetry.id"))
-    status = Column(Enum(MovieStatus))
+    status = Column(Enum(MovieStatus), default=MovieStatus.MOVIE_STATUS_NEW)
     error_message = Column(Text)
-    discharge = Column(Float)
+    discharge_q05 = Column(Float)
+    discharge_q25 = Column(Float)
+    discharge_q50 = Column(Float)
+    discharge_q75 = Column(Float)
+    discharge_q95 = Column(Float)
 
     config = relationship("CameraConfig")
     bathymetry = relationship("Bathymetry")
@@ -40,3 +48,34 @@ class Movie(Base, SerializerMixin):
 
     def __repr__(self):
         return "{}: {}".format(self.id, self.__str__())
+
+
+@event.listens_for(Movie, 'after_insert')
+@event.listens_for(Movie, 'after_update')
+def receive_after_update(mapper, connection, target):
+    if target.status == MovieStatus.MOVIE_STATUS_NEW.name:
+        print('Queue extract task for movie {}'.format(target.id))
+        queue_task("extract_frames", target)
+    elif target.status == MovieStatus.MOVIE_STATUS_READY.name and target.actual_water_level is not None:
+        print('Queue run task for movie {}'.format(target.id))
+        queue_task("run", target)
+
+def queue_task(type, movie):
+    connection = pika.BlockingConnection(
+        pika.URLParameters(os.getenv("AMQP_CONNECTION_STRING"))
+    )
+    channel = connection.channel()
+    channel.queue_declare(queue="processing")
+    channel.basic_publish(
+        exchange="",
+        routing_key="processing",
+        body=json.dumps({"type": type, "kwargs": {"movie": get_task_json(movie)}})
+    )
+    connection.close()
+
+def get_task_json(movie):
+    movie_example['id'] = movie.id
+    if movie.actual_water_level is not None:
+        # Decimal can't be JSON encoded with default encoder.
+        movie_example['h_a'] = float(movie.actual_water_level)
+    return movie_example
